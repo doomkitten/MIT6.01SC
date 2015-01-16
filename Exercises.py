2fae5a8c6684e0ebe2d167f1e2306c4d963c7291
doomkitten <jones.alaina@gmail.com>
2015-01-15 23:07:19 -0600

    Updated Exercises with Tests

--- Exercises.py
+++ Exercises.py
@@ -1,41 +1,44 @@
 #MIT.601SC Exercises
-#These are are my answers to exercise questions from MIT's 6.01SC
-#These exercises helped me refresh on my python syntax
-#For more information regarding these problems, see
+#My answers to exercise questions from MIT 6.01SC
+#Exercises were used as a python syntax refresher
+#For more information regarding the problem set, see the exercise .pdf at
 #http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-01sc-introduction-to-electrical-engineering-and-computer-science-i-spring-2011/unit-1-software-engineering/object-oriented-programming/MIT6_01SCS11_chap02.pdf
 
+#Typical Fibonacci Sequence Example	
+#fib(n) returns the nth Fibonacci number.
+def fib(n):
+	if n <= 0: return 0
+	elif n == 1: return 1
+	return fib(n-1) + fib (n-2)
 
 #Exercise 2.1:
 #Write a procedure that takes a list of numbers (nums) and a limit (limit)
-#and returns a list which is the shortest prefix of nums the sum of whose
-#values is greater than limit. Use a for loop. 
+#and returns a list which is the shortest subset of nums whose
+#sum is greater than limit. Use a for loop. 
 
 #None is returned if the sum of the list is less than the limit
-def numberList(nums, limit):
+def sumToLimit(nums, limit):
 	nums.sort()
 	nums.reverse()
 	sum = 0
-	i = 0
-	for x in nums:
+	for index, x in enumerate(nums):
 		if sum >= limit:
-			return nums[:i]
+			return nums[:index]
 		else:
 			sum = sum + x
-			i += 1
 	return None;	
 
 
 
 #Exercise 2.2	
 #Write a procedure that takes n as an argument and returns the sum of the
-#squares of the integers from 1 to n-1. It should use for and range. 
+#squares of the integers from 1 to n. It should use for and range. 
 
 #Iterative Defintion of 2.2:
 def sumOfSquaresIterative(n):
-	rangeOfNums = range(n)
 	sum = 0
-	for x in rangeOfNums:
-		sum = sum + x**2
+	for x in range(n+1):
+		sum += x**2
 	return sum
 	
 #Recursive Definition of 2.2: 
@@ -43,7 +46,7 @@ def sumOfSquaresIterative(n):
 def sumOfSquaresRecursive(n):
 	if n == 1:
 		return 1
-	return n*n + sumOfSquares(n-1)
+	return n**2 + sumOfSquaresRecursive(n-1)
 	
 
 #Exercise 2.4. 
@@ -82,11 +85,18 @@ def isInSet(a,b):
 		return True
 	return isInSet(a,b[1:])
 	
-#Typical Fibonacci Sequence Example	
-#fib(n) returns the nth Fibonacci number.
-def fib(n):
-	if n <= 0: return 0
-	elif n == 1: return 1
-	return fib(n-1) + fib (n-2)
+			
+			
+#Test
+a = [14, 62, 3, 1]
+b = [14, 62]
+c = [14, 13, 62]
+print sumToLimit(a, 64)
+print sumOfSquaresIterative(25)
+print sumOfSquaresRecursive(25)
+print isSubset(b,a)
+print isSubset(c,b)
+print fib(6)
+
 
 
